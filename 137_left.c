772f29e68c52fb106b5be1c2b9f4ed6be4ac9574
YuzeZhang <zhangyuzechn@foxmail.com>
2019-08-08 21:58:00 +0800

    Submit today's C practice.--190808

---
+++ 137_left.c
@@ -0,0 +1,10 @@
+//
+// Created by zeno on 2019/8/8.
+//
+
+#include <stdio.h>
+int main(){
+    int x;
+    x = 1<<3+2;
+    printf("%d",x);
+}
---
+++ 138_short_circuit.c
@@ -0,0 +1,13 @@
+//
+// Created by zeno on 2019/8/8.
+//
+
+#include <stdio.h>
+int main(){
+    int a,x;
+    for(a = 0,x = 0;a<=1&&!x++;a++){
+        a++;
+    }
+    printf("%d %d",a,x);
+    return 0;
+}
