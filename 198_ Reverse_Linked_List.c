646b8ecc8e09b41682c295cdac917d3ae07da5ff
YuzeZhang <zhangyuzechn@foxmail.com>
2019-10-03 16:46:34 +0800

    commit

---
+++ 198_ Reverse_Linked_List.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+
+}
+
