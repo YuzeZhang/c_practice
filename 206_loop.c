87c3c9885028653734d05d61687abcbd53d0aa85
YuzeZhang <zhangyuzechn@foxmail.com>
2019-12-03 21:04:02 +0800

    add --2019/12/3

---
+++ 206_loop.c
@@ -0,0 +1,15 @@
+//
+// Created by Zeno on 2019/12/3.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+    while (1){
+        printf("hello\n");
+        sleep(1);
+    }
+    return 0;
+}
